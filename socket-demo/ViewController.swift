4c703a9479d3a7564465fdfa412557dde22fa0ac
Nil Nguyen <nilverspace@gmail.com>
2022-02-19 14:49:11 +0700

    Initial Commit

---
+++ socket-demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  socket-demo
+//
+//  Created by LAP11353 on 19/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
